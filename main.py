0c764700eff37714735bcbe814320d17603bf71d
vaderclarity <136130359+vaderclarity@users.noreply.github.com>
2025-02-27 17:56:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://frontend-azure-theta.vercel.app"],
     allow_credentials=True,
     allow_methods=["POST"],
     allow_headers=["*"],
